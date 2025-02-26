29bd99934c5eae5c1a53b103b8913e75f324694c
Oliver Gingerich <Ogingerich5354@conestogac.on.ca>
2025-02-25 20:02:05 -0500

    Finished fixing NavBar functionality.

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -9,15 +9,15 @@ const Navbar = () => {
             </div>
             <div className="navbar-right">
                 <ul className="nav-links">
-                    <li>
-                        <a href="/about">About</a>
-                    </li>
-                    <li>
-                        <a href="/projects">Projects</a>
-                    </li>
-                    <li>
-                        <a href="/contact">Contact</a>
-                    </li>
+                <li>
+                    <Link to={"/about"}>About</Link>
+                </li>
+                <li>
+                    <Link to={"/projects"}>Projects</Link>
+                </li>
+                <li>
+                    <Link to={"/contact"}>Contact</Link>
+                </li>
                 </ul>
             </div>
         </nav>
