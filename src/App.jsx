2fb86d5c249c92c46b25c6f7fbbacaae56181711
Oliver Gingerich <Ogingerich5354@conestogac.on.ca>
2025-02-26 20:56:29 -0500

    Added links and images to home page.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ import viteLogo from '/vite.svg'
 import Navbar from './Navbar'
 import MobileNav from './MobileNav'
 import './App.css'
+import linkedin from './logos/linkedin.png'
+import github from './logos/github.png'
+import email from './logos/email.png'
+import x from './logos/x.png'
+import instagram from './logos/instagram.png'
+import tiktok from './logos/tiktok.png'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,7 +24,32 @@ function App() {
       <div className='content'>
         <h1 className="title">Oliver Gingerich</h1>
         <div className='info'>
-          <h3>Programming | Writing | Art</h3>
+          <h3>Software Student | Programming | Writing | Art</h3>
+        </div>
+        <div className='logos'>
+          <a href='https://www.linkedin.com/in/oliver-gingerich/'>
+            <img className='logo' src={linkedin} alt='Linkedin'/>
+          </a>
+
+          <a href='https://github.com/Oliver-Gi'>
+            <img className='logo' src={github} alt='GitHub'/>
+          </a>
+
+          <a href='mailto:ogingerich5354@conestogac.on.ca'>
+            <img className='logo' src={email} alt='Email'/>
+          </a>
+
+          <a href='https://x.com/Oliver__Gi'>
+            <img className='logo' src={x} alt='X'/>
+          </a>
+
+          <a href='https://www.instagram.com/oliver__gi/'>
+            <img className='logo' src={instagram} alt='Instagram'/>
+          </a>
+
+          <a href='https://www.tiktok.com/@oliver__gi'>
+            <img className='logo' src={tiktok} alt='Tiktok'/>
+          </a>
         </div>
       </div>
     </>
