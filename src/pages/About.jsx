7a7a3463c3cfa2c65f52d5a95f4a31c4e37d1a7f
Oliver Gingerich <Ogingerich5354@conestogac.on.ca>
2025-02-27 15:01:56 -0500

    Designed about page.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,63 @@
 import React from 'react'
 import Navbar from '../Navbar'
 import MobileNav from '../MobileNav'
+import './Pages.css'
+import aboutMePic from '../logos/aboutMePic.jpg'
 
 const About = () => {
     return(
         <div>
             <Navbar/>
             <MobileNav/>
-            <h1>This is about page</h1>
+            <h1>About Me</h1>
+            <div className='contentAbout'>
+                <div className='left'>
+                    {/*Add photo here!*/}
+                    <img className='aboutMeImage' src={aboutMePic} alt='Picture of Me'/>
+                    <h3>Hey, I'm Oliver, a second year Software Engineering Technology student at Conestoga College from Ontario, Canada!<br/>
+                        My passion is in frontend development and web design, I love learning and I'm excited to continue building on my skills!
+                    </h3>
+                </div>
+
+                <div className='line'></div>
+
+                <div className='right'>
+                    <h2>Skills</h2>
+                    <div className='skillBox'>
+                        <ul className='skill'>
+                            <li>Web Development</li>
+                            <li>Frontend Development</li>
+                            <li>JavaScript</li>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>ASP.NET</li>
+                            <li>PHP</li>
+                            <li>C/C++/C#</li>
+                            <li>Java</li>
+                            <li>Python</li>
+                            <li>.NET Framework</li>
+                            <li>Data Structures</li>
+                            <li>SQL</li>
+                            <li>Databases</li>
+                            <li>SDLC Lifecycle</li>
+                            <li>Agile Development</li>
+                        </ul>
+                    </div>
+
+                    <h2>Hobbies</h2>
+                    <div className='skillBox'>
+                        <ul className='skill'>
+                            <li>Programming</li>
+                            <li>Writing</li>
+                            <li>Art</li>
+                            <li>Music</li>
+                            <li>Anime</li>
+                            <li>Magic The Gathering</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <br/>
         </div>
     )
 }
