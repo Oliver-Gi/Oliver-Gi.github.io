5a6089cf4a837c7bc87ccac14f166e5554ec7ffd
Oliver Gingerich <Ogingerich5354@conestogac.on.ca>
2025-03-16 23:45:18 -0400

    Trying to fix router.

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {
-  createBrowserRouter,
+  BrowserRouter,
   RouterProvider,
 } from 'react-router-dom'
 import './index.css'
