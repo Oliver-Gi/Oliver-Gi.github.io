15cde66131a4790044721e3a39b43226fa7fee81
Oliver Gingerich <Ogingerich5354@conestogac.on.ca>
2025-03-17 01:00:17 -0400

    Fixed routing issues
    Converted from BrowserRouter to HashRouter to avoid error 404. Created
    custom error 404 incase users forget the #/ in the URL.

--- src/main.jsx
+++ src/main.jsx
@@ -2,31 +2,27 @@ import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {
-  BrowserRouter,
-  RouterProvider,
+  HashRouter, Routes, Route, Outlet, Link
 } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import About from './pages/About.jsx'
 import Projects from './pages/Projects.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: "",
-    element: <App/>,
-  },
-  {
-    path: "/about",
-    element: <About/>,
-  },
-  {
-    path: "/projects",
-    element: <Projects/>,
-  },
-])
+export default function Direct(){
+  return(
+    <HashRouter>
+      <Routes>
+          <Route index element={<App/>}/>
+          <Route path="about" element={<About/>}/>
+          <Route path="projects" element={<Projects/>}/>
+      </Routes>
+    </HashRouter>
+  )
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <Direct/>
   </StrictMode>,
 )
